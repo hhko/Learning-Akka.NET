e0387724c6a970d94d6f4e8d55efe17d5d94d38c
Hyungho.Ko <hyungho.ko@gmail.com>
2019-05-01 02:37:11 +0900

    Pub/Sub 모든 Topic을 얻는 Lab을 추가한다.

---
+++ ClusterLab/Ch 07. Distributed PubSub/04. Publish - Get Topics/NonSeedNode3/PublisherActor.cs
@@ -0,0 +1,52 @@
+﻿using Akka.Actor;
+using Akka.Cluster;
+using Akka.Cluster.Tools.PublishSubscribe;
+using Akka.Event;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace NonSeedNode3
+{
+    public class PublisherActor : ReceiveActor
+    {
+        #region Messages
+        public sealed class TellSend { }
+        #endregion
+
+        private readonly ILoggingAdapter _log = Context.GetLogger();
+
+        public static Props Props()
+        {
+            return Akka.Actor.Props.Create(() => new PublisherActor());
+        }
+
+        public PublisherActor()
+        {
+            Context.System.Scheduler.ScheduleTellOnce(TimeSpan.FromSeconds(6), Self, new TellSend(), Self);
+
+            Receive<TellSend>(_ => Handle(_));
+            Receive<CurrentTopics>(_ => Handle(_));
+        }
+
+        private void Handle(TellSend msg)
+        {
+            _log.Info($">>> Recevied message : {msg}, Sender: {Sender}");
+
+            var mediator = DistributedPubSub.Get(Context.System).Mediator;
+
+            mediator.Tell(GetTopics.Instance);
+        }
+
+        private void Handle(CurrentTopics msg)
+        {
+            _log.Info($">>> Recevied message : {msg}, Sender: {Sender}");
+
+            foreach (string currentTopic in msg.Topics)
+            {
+                _log.Info($">>> Current Topic : {currentTopic}");
+            }
+        }
+    }
+}
