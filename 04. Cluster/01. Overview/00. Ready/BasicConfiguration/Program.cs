da751fe06820b584604041c0c5e1b9cc300260bb
hhko <hyungho.ko@gmail.com>
2019-12-21 17:22:29 +0900

    기본 구성을 위한 예제 추가

---
+++ 04. Cluster/01. Overview/00. Ready/BasicConfiguration/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasicConfiguration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
